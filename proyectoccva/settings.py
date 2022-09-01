df6589344967025cf7556c2b943a58daa70110e8
turuars <111399355+turuars@users.noreply.github.com>
2022-08-31 21:38:31 -0300

    Update settings.py

--- proyectoccva/settings.py
+++ proyectoccva/settings.py
@@ -56,13 +56,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 
 ]
 
@@ -174,6 +174,7 @@ if ENVIRONMENT== "development":
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
